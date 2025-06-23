094c114b1097cd639524128c34f58476cfdd2dc2
ahmed.elsobky <ahmed.elsobky@SW-A-ELSOBKY>
2025-06-23 14:28:00 +0300

    edit baskitItem Entity

--- Ecom.core/Entites/BasketItem.cs
+++ Ecom.core/Entites/BasketItem.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { set; get; }
 
+        public string Description { set; get; }
         public string Image { get; set; }
         public int Quentity { get; set; }
         public decimal Price { set; get; }
